92811dee4f85e185a997b9ae1554edf37e645a23
elemonier <emily.lemonier@gmail.com>
2013-09-10 11:36:35 -0400

    Full rotation around block functionality. Need to check edge cases.

--- ControlProgram1.m
+++ ControlProgram1.m
@@ -14,6 +14,9 @@ function finalRad= ControlProgram1(serPort)
 % finalRad - Double, final turning radius of the Create (m)
 
 
+%distance sensor rumba - check total distance from Start
+%check edge cases
+
     % Set constants for this program
     maxDuration= 1200;  % Max time to allow the program to run (s)
     maxDistSansBump= 5; % Max distance to travel without obstacles (m)
@@ -35,27 +38,78 @@ function finalRad= ControlProgram1(serPort)
 
     % Start robot moving
     SetFwdVelAngVelCreate(serPort,v,w);
-    
-    %reset algo
+    initialHit = 1;  
+    forward = 0;
+    turnAngle = 0;
     while (toc(tStart) < maxDuration)
-        initialHit = 1;
-        initialBump = 1;
+        bumped = bumpChecks(serPort);
+        frontBump = frontBumpCheck(serPort);
 
-        %wall encounter
-        bumped = bumpCheck(serPort);
+        display(bumped);
+        %display(frontBump);
+        %display(turnAngle);
+        %waiting for first hit
+        if initialHit
+            initialHit = firstBump(serPort);
+        end
         
-        if bumped && initialHit
-            Reset(serPort);
-            initialHit = 1;
+        if ~initialHit 
+            if ~forward
+                %bump check ~ reset
+                forward = bumpCheck(serPort)
+                turnAngle = 0;
+            end
+
+            if forward
+                if bumped && ~frontBump
+                    SetFwdVelAngVelCreate(serPort,v,w);
+                    turnAngle = 0;
+                elseif bumped && frontBump
+                    bumpCheck(serPort);
+                    turnAngle = 0;
+                else ~bumped
+                    SetFwdVelAngVelCreate(serPort, 0, -pi/8);
+                    turnAngle = turnAngle + pi/8;
+                    %if(turnAngle >= pi/2)
+                    %    SetFwdVelAngVelCreate(serPort, 1, -pi/8);
+                    %end
+                    %check for 180 deg turn, move over + down
+                    %forward = 0;
+                    % Wait for turn to complete
+                    %angTurned= 0;
+                    %ang = pi/4;
+                    %AngleSensorRoomba(serPort);
+                    %while angTurned < ang
+                    %    angTurned= angTurned+abs(AngleSensorRoomba(serPort));
+                    %    pause(0.1)
+                end
+                    %SetFwdVelAngVelCreate(serPort,v,w);
+                %end
+            end
+
+
         end
 
-        if ~bumped && initialBump
-            turnAngle(serPort, .5, -pi/8);
-            SetFwdVelAngVelCreate(serPort,v,w);
-            initialBump = 0;
-        else ~bumped && ~initialBump
-            SetFwdVelAngVelCreate(serPort,v,w);
-        end
+        
+        %no hit
+        %if bumped
+        %    SetFwdVelAngVelCreate(serPort,v,w);
+        %    initialHit = 0;
+        %end
+       
+        %forward hit
+        %if bumped == 2
+        %    Reset(serPort);
+        %end
+
+        %if bumped == 0 && ~initialBump
+        %    turnAngle(serPort, .5, -pi/8);
+        %    SetFwdVelAngVelCreate(serPort,v,w);
+        %    initialBump = 0;
+        %end
+        %else ~bumped && ~initialBump
+        %    SetFwdVelAngVelCreate(serPort,v,w);
+        %end
         
         %force right bump
         %if a
@@ -78,7 +132,7 @@ function finalRad= ControlProgram1(serPort)
     pause(0.1);
     end
     
-   % turnAngle(serPort, .5, -pi/8);
+    % turnAngle(serPort, .5, -pi/8);
     %
     %leftbump = forwardCheck(serPort)
     %while(leftbump = forwardCheck(serPort))
@@ -101,8 +155,27 @@ function finalRad= ControlProgram1(serPort)
     % Don't use these if you call RoombaInit prior to the control program
 end
 
+function initialHit = firstBump(serPort)
+% Check bump sensors and steer the robot away from obstacles if necessary
+%
+% Input:
+% serPort - Serial port object, used for communicating over bluetooth
+%
+% Output:
+% leftbump - Boolean, true if bump sensor is activated
 
-function bumped = bumpCheck(serPort)
+    % Check bump sensors (ignore wheel drop sensors)
+    [BumpRight BumpLeft WheDropRight WheDropLeft WheDropCaster ...
+        BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
+    if BumpFront || BumpRight || BumpLeft
+        initialHit = 0;
+    else
+        SetFwdVelAngVelCreate(serPort,.5,0);
+        initialHit = 1;
+    end
+end
+
+function bumped = bumpChecks(serPort)
 % Check bump sensors and steer the robot away from obstacles if necessary
 %
 % Input:
@@ -118,7 +191,23 @@ function bumped = bumpCheck(serPort)
 
 end
 
-function rightbump = rightCheck(serPort)
+function frontBump = frontBumpCheck(serPort)
+% Check bump sensors and steer the robot away from obstacles if necessary
+%
+% Input:
+% serPort - Serial port object, used for communicating over bluetooth
+%
+% Output:
+% bumped - Boolean, true if bump sensor is activated
+
+    % Check bump sensors (ignore wheel drop sensors)
+    [BumpRight BumpLeft WheDropRight WheDropLeft WheDropCaster ...
+        BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
+    frontBump= BumpFront;
+
+end
+
+function forward = bumpCheck(serPort, initialHit)
 % Check bump sensors and steer the robot away from obstacles if necessary
 %
 % Input:
@@ -130,9 +219,18 @@ function rightbump = rightCheck(serPort)
     % Check bump sensors (ignore wheel drop sensors)
     [BumpRight BumpLeft WheDropRight WheDropLeft WheDropCaster ...
         BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
-    rightbump= BumpRight;
-end
+    if BumpFront
+        turnAngle(serPort, .2, pi/5);
+        forward = 0;
+    elseif BumpRight
+        turnAngle(serPort, .2, pi/5);
+        forward = 0;
+    else
+        turnAngle(serPort, .2, -pi/5);
+        forward = 1;
+    end
 
+end
 
 function w= v2w(v)
 % Calculate the maximum allowable angular velocity from the linear velocity
@@ -164,7 +262,37 @@ function Reset(serPort)
         turnAngle(serPort, .2, pi/8);
         bumped = bumpCheck(serPort);
     end
-    a = 1;
+end
+
+function findWall(serPort)
+% Calculate the maximum allowable angular velocity from the linear velocity
+%
+% Input:
+% v - Forward velocity of Create (m/s)
+%
+% Output:
+% w - Angular velocity of Create (rad/s)
+    bumped = bumpCheck(serPort);
+    while bumped == 0
+        SetFwdVelAngVelCreate(serPort,0,0); 
+        bumped = bumpCheck(serPort); 
+    end  
+end
+
+function moveForward(serPort)
+% Calculate the maximum allowable angular velocity from the linear velocity
+%
+% Input:
+% v - Forward velocity of Create (m/s)
+%
+% Output:
+% w - Angular velocity of Create (rad/s)
+    turnAngle(serPort, .2, -pi/8);
+    bumped = bumpCheck(serPort);
+    while bumped == 1
+     SetFwdVelAngVelCreate(serPort,0,0);       
+     bumped = bumpCheck(serPort);
+    end
 end
 
 
